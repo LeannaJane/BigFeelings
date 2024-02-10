68f8bfdfacaae1ce6cec6859669106929dc64ac0
Leannaaaa <91910570+Leannaaaa@users.noreply.github.com>
2024-02-10 01:08:49 +0000

    Updating the floating buttons with correct icons.

--- big_feelings/lib/Classes/floating_buttons_bar.dart
+++ big_feelings/lib/Classes/floating_buttons_bar.dart
@@ -12,10 +12,11 @@ class FloatingButtonsBar extends StatelessWidget {
     super.key,
     this.height = 50.0,
     this.width =
-        50.0, // This is the height and width of the floating buttons, all the buttons have the same width and height for consistency.
-    this.bottomOffset = 40.0, // This is the vertical offset
+        50.0, //!This is the height and width of the floating buttons, all the buttons have the same width and height for consistency.
+    //! This is the bottom offset
+    this.bottomOffset = 40.0,
     this.leftOffset =
-        16.0, // This is the horizontal offset for left and right buttons
+        16.0, //! This is the horizontal offset for left and right buttons
   });
 
   @override
@@ -23,53 +24,58 @@ class FloatingButtonsBar extends StatelessWidget {
     return Consumer<ThemeNotifier>(builder: (context, themeNotifier, child) {
       final currentTheme = themeNotifier.currentTheme;
 
-      // Define the color for the buttons based on the current theme
+      //! Define the color for the buttons based on the current theme
       Color buttonColor = currentTheme == ThemeNotifier.darkTheme
-          ? Colors.grey[800]! // Set the color to grey for dark theme
-          : Colors.white; // Set the color to white for light theme
+          ? Colors.grey[800]! //! Set the colour to grey for dark theme
+          : Colors.white; //! Set the colour to white for light theme
 
-      // Define the icon color based on the current theme
+      //! Define the icon color based on the current theme
       Color iconColor = currentTheme == ThemeNotifier.darkTheme
-          ? Colors.white // Set the icon color to white for dark theme
-          : Colors.black; // Set the icon color to black for light theme
+          ? Colors.white //! Set the icon colour to white for dark theme
+          : Colors.black; //! Set the icon colour to black for light theme
 
       return Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // Changed to a sized box to fix the a container error. As they were uncessasry.
+          //! Changed to a sized box to fix the a container error. As they were uncessasry.
           SizedBox(
             width: width,
             height: height,
             child: FloatingActionButton(
               onPressed: () {},
+              //! Shirt icon for clothing features for the pet.
               tooltip: 'Shirt',
               heroTag: 'btn1',
               backgroundColor: buttonColor,
               child: Icon(Icons.shopping_bag, color: iconColor),
             ),
           ),
-          SizedBox(width: leftOffset), // Space between buttons
+          //! Space between buttons
+          SizedBox(width: leftOffset),
           SizedBox(
             width: width,
             height: height,
             child: FloatingActionButton(
               onPressed: () {},
+              //! Heart icon to represent love to the pet.
               tooltip: 'Heart',
               heroTag: 'btn2',
               backgroundColor: buttonColor,
-              child: Icon(Icons.shopping_bag, color: iconColor),
+              child: Icon(Icons.favorite, color: iconColor),
             ),
           ),
-          SizedBox(width: leftOffset), // Space between buttons
+          //! Space between buttons
+          SizedBox(width: leftOffset),
           SizedBox(
             width: width,
             height: height,
             child: FloatingActionButton(
               onPressed: () {},
+              //! Food icon to represent feeding the pet.
               tooltip: 'Fork and Knife',
               heroTag: 'btn3',
               backgroundColor: buttonColor,
-              child: Icon(Icons.shopping_bag, color: iconColor),
+              child: Icon(Icons.fastfood, color: iconColor),
             ),
           ),
         ],
