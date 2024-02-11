003ab493ccd10e4f82aa0bb3f974f5c8f9494b02
Leannaaaa <91910570+Leannaaaa@users.noreply.github.com>
2024-02-11 14:14:08 +0000

    Adding logging references to the references.dart file. This is to keep the code less messy as I am continuously changing stuff, and having to rewrite comments and references.

---
+++ big_feelings/lib/References/References.dart
@@ -0,0 +1,37 @@
+// ! Welcome to the References File!
+//! This file contains all the references and citations used in this project.
+//* --------------------------------------------------------------------------------------------
+
+
+//! References from the Login page.
+
+/*
+1
+? docs.flutter.dev. (n.d.). Display a snackbar. [online] Available at: https://docs.flutter.dev/cookbook/design/snackbars [Accessed 3 Feb. 2024].
+* - This website provided guidance on displaying a snackbar in Flutter and assisted me in creating a snackbar.
+2
+? stack Overflow. (n.d.). Do not use BuildContexts across async gaps. [online] Available at: https://stackoverflow.com/questions/68871880/do-not-use-buildcontexts-across-async-gaps [Accessed 5 Feb. 2024].
+* - This website was used to help guide me on how to handle the 'BuildContext' across async.
+* - It told me to use the mounted property to check if the widgets are still mounted before performing actions asynchronously.
+3
+?  Kumar, Y. (2022). Login Page UI in Flutter. [online] Medium. Available at: https://levelup.gitconnected.com/login-page-ui-in-flutter-65210e7a6c90.
+* - This login page was origionally a template guided by the flutter webpage, and was eventually updated to be my own login page.
+* - This login page was used for inspiration that guided me to create a basic login, then I improved it making it my own.
+4
+?  Stack Overflow. (n.d.). Flutter Text Field: How to add Icon inside the border. [online] Available at: https://stackoverflow.com/questions/55929495/flutter-text-field-how-to-add-icon-inside-the-border [Accessed 3 Feb. 2024].
+*  This page helped me understand how to add an icon inside the border.
+5
+? Firebase. (n.d.). Error Handling | Firebase Documentation. [online] Available at: https://firebase.google.com/docs/auth/flutter/errors.
+* - This was used to help me understand how to use a if statement for the error codes.
+* if (e.code == 'account-exists-with-different-credential') {
+* This line assisted me in creating my own if statements, but I had to try create errors when testing my login to see what errors and place the error in the if statement.
+6
+? somniosoftware.com. (n.d.). Email Authentication with Flutter - Firebase. [online] Available at: https://www.somniosoftware.com/blog/email-authentication-with-flutter-firebase [Accessed 11 Feb. 2024].
+* This website guided me on how to create a email login and a create an account page.
+
+? www.youtube.com. (n.d.). 📱Login & Logout • Firebase x Flutter Tutorial ♡. [online] Available at: https://www.youtube.com/watch?v=TkuO8OLgvkk
+* This video guided me on how to create a login and logout using firebase and the email controller and password controller. 
+*/
+
+
+
