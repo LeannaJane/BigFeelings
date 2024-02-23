98a36dc858692d21730859be84db24d72c1ddae6
Leannaaaa <91910570+Leannaaaa@users.noreply.github.com>
2024-02-23 12:57:54 +0000

    Adding outline to the image and text underneath it.

--- big_feelings/lib/Pages/Mood Tracker/mood_tracker_page.dart
+++ big_feelings/lib/Pages/Mood Tracker/mood_tracker_page.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
-class MoodTrackerPage extends StatelessWidget {
-  // ignore: use_key_in_widget_constructors
-  const MoodTrackerPage({Key? key});
+class MoodTrackerPage extends StatefulWidget {
+  const MoodTrackerPage({Key? key}) : super(key: key);
+
+  @override
+  _MoodTrackerPageState createState() => _MoodTrackerPageState();
+}
+
+class _MoodTrackerPageState extends State<MoodTrackerPage> {
+  late int selectedEmotionIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    //! This Initialises the selected emotion index to -1  which means that no emotion is selected
+    selectedEmotionIndex = -1;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +32,8 @@ class MoodTrackerPage extends StatelessWidget {
      */
     return Scaffold(
       appBar: AppBar(
-        title: const Text('How do you feel?'), // Change the title
+        //! I changed the title to how do you feel, as this page is going to be a option page, so that the user can place an amotion and check their emotions.
+        title: const Text('How do you feel?'),
       ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -27,56 +41,117 @@ class MoodTrackerPage extends StatelessWidget {
           mainAxisSpacing: 10.0,
           crossAxisSpacing: 10.0,
         ),
-        itemCount: 12, // Total number of emotion images
+        //! Total number of emotion images
+        itemCount: 12,
         itemBuilder: (context, index) {
           String imagePath = 'assets/images/images_mood/';
+          //! Adding an emotion name to go under each emotional image.
+          String emotionName = '';
 
           switch (index) {
             case 0:
               imagePath += 'angry.png';
+              emotionName = 'Angry';
               break;
             case 1:
               imagePath += 'happy.png';
+              emotionName = 'Happy';
               break;
             case 2:
               imagePath += 'Ok.png';
+              emotionName = 'Ok';
               break;
             case 3:
               imagePath += 'overwhelmed.png';
+              emotionName = 'Overwhelmed';
               break;
             case 4:
               imagePath += 'sad.png';
+              emotionName = 'Sad';
               break;
             case 5:
               imagePath += 'sick.png';
+              emotionName = 'Sick';
               break;
             case 6:
               imagePath += 'Silly.png';
+              emotionName = 'Silly';
               break;
             case 7:
               imagePath += 'surprised.png';
+              emotionName = 'Surprised';
               break;
             case 8:
               imagePath += 'tired.png';
+              emotionName = 'Tired';
               break;
             case 9:
               imagePath += 'unhappy.png';
+              emotionName = 'Unhappy';
               break;
             case 10:
               imagePath += 'upset.png';
+              emotionName = 'Upset';
               break;
             case 11:
               imagePath += 'lonely.png';
+              emotionName = 'Lonely';
               break;
           }
 
           return GestureDetector(
             onTap: () {
-              logger.i('Emotion ${index + 1} selected');
+              setState(() {
+                //! This checks if the tapped emotion is already selected
+                if (selectedEmotionIndex == index) {
+                  //! If its already selected and clicked again it will unselect.
+                  selectedEmotionIndex = -1;
+                  logger.i('Emotion ${index + 1} unselected');
+                } else {
+                  //! If it's not selected, unselect all other emotions and select the tapped one
+                  //! Only one emotion can be selected at a time.
+                  selectedEmotionIndex = index;
+                  logger.i('Emotion ${index + 1} selected');
+                }
+              });
             },
-            child: Image.asset(
-              imagePath,
-              fit: BoxFit.cover,
+            //! This adds a border by adding a selected box to the image, and when the image is selected the boarder will show on the selected image.
+            child: SizedBox(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    //! Wrapped the column with Expanded due to exceptions.
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: selectedEmotionIndex == index
+                              ? Colors.black
+                              : Colors.transparent,
+                          width: 2.0,
+                        ),
+                      ),
+                      child: Image.asset(
+                        imagePath,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  //! Adding space between the image and text underneath.
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    //! Then adding the emotion name underneath the image emotion.
+                    emotionName,
+                    //! This will eventually be changed.
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
