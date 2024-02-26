0f3b9784a9c96611d7755a6ce07618514cc6af7c
Leannaaaa <91910570+Leannaaaa@users.noreply.github.com>
2024-02-26 21:56:22 +0000

    Basic mood entry page.

---
+++ big_feelings/lib/Pages/Mood Tracker/mood_entires_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MoodEntriesPage extends StatelessWidget {
+  const MoodEntriesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mood entries page.'),
+      ),
+    );
+  }
+}
