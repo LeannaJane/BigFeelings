ccb88bb5830f00197108960882fa5d3f3f98b0c5
Leanna <leannajane2002@outlook.com>
2024-03-14 01:49:32 +0000

    Quiz 2 option page.

--- big_feelings/lib/Pages/Quizzes/quiz_two.dart
+++ big_feelings/lib/Pages/Quizzes/quiz_two.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:big_feelings/Classes/theme_notifier.dart'; // Add this import
 import 'package:big_feelings/Classes/font_provider.dart'; // Add this import
 
@@ -20,7 +19,6 @@ class _StartQuiz2State extends State<StartQuiz2> {
       Color backgroundColor = currentTheme == ThemeNotifier.darkTheme
           ? Colors.grey[800]!
           : Colors.white;
-      final User? user = FirebaseAuth.instance.currentUser;
       Color iconColor = themeNotifier.getIconColor();
 
       return Scaffold(
