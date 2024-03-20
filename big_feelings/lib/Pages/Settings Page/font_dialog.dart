936a97d1b281ba99440f66e9b4cccc61273540cf
Leanna <leannajane2002@outlook.com>
2024-03-20 21:52:35 +0000

    Updating font colours and background colour for alert dialog.

--- big_feelings/lib/Pages/Settings Page/font_dialog.dart
+++ big_feelings/lib/Pages/Settings Page/font_dialog.dart
@@ -1,58 +1,36 @@
+// ignore_for_file: use_super_parameters
+
 import 'package:big_feelings/Classes/font_provider.dart';
+import 'package:big_feelings/Classes/theme_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-/*
-! Reference
-* Android Developers. (n.d.). Work with fonts | Jetpack Compose. [online] Available at: https://developer.android.com/jetpack/compose/text/fonts [Accessed 3 Feb. 2024].
-* This website assisted in me setting fonts.
-
-* www.youtube.com. (n.d.). Custom Font in Flutter | Flutter external font | Flutter Tutorial #14. [online] Available at: https://www.youtube.com/watch?v=qI_7znKKlhA [Accessed 3 Feb. 2024].
-
-* www.youtube.com. (n.d.). Flutter Tutorial - Full Course - Project Based. [online] Available at: https://www.youtube.com/watch?v=OO_-MbnXQzY [Accessed 3 Feb. 2024].
-
-* docs.flutter.dev. (n.d.). Work with long lists. [online] Available at: https://docs.flutter.dev/cookbook/lists/long-lists.
-
-* The flutter website helped me into creating a list.
-‌
-* api.flutter.dev. (n.d.). actions property - AppBar class - material library - Dart API. [online] Available at: https://api.flutter.dev/flutter/material/AppBar/actions.html [Accessed 6 Feb. 2024].
-* Implemented an action widget using the code provided by flutter to assist me.
-‌   
-* I had an issue where the fonts were not updating to the selected font on the drop down, so this website helped me understand how to use an alert dialog.
-* Stack Overflow. (n.d.). How to refresh an AlertDialog in Flutter? [online] Available at: https://stackoverflow.com/questions/51962272/how-to-refresh-an-alertdialog-in-flutter [Accessed 6 Feb. 2024].
-* This website also helped me understand how to write my show dialog dropdown using a stateful builder.
-‌  
-
-* I used this website to understand how to change the opacity of the dimming in the background the line:
-* backgroundColor: Colors.white.withOpacity(0.85),
-* Stack Overflow. (n.d.). How to make a full screen dialog in flutter? [online] Available at: https://stackoverflow.com/questions/51908187/how-to-make-a-full-screen-dialog-in-flutter [Accessed 6 Feb. 2024].
-*/
 class FontDropdownDialog extends StatelessWidget {
-  // ignore: use_super_parameters
   const FontDropdownDialog({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<FontProvider>(
-      builder: (context, fontProvider, child) {
+    return Consumer2<FontProvider, ThemeNotifier>(
+      builder: (context, fontProvider, themeNotifier, child) {
         final selectedFontFamily = fontProvider.selectedFontFamily;
-
+        Color getContainerColor = themeNotifier.getContainerColor();
         return Center(
-          // Wrap AlertDialog with Center
           child: SizedBox(
-            width: 350.0, // Fixed width for AlertDialog
-            height: 400.0, // Fixed height for AlertDialog
+            width: 350.0,
+            height: 400.0,
             child: SingleChildScrollView(
-              physics:
-                  const NeverScrollableScrollPhysics(), // Disable scrolling
-              // Wrap with SingleChildScrollView
+              physics: const NeverScrollableScrollPhysics(),
               child: AlertDialog(
+                backgroundColor: getContainerColor,
                 title: Text(
                   'Select a font:',
-                  style: fontProvider.getSubTitleStyle(),
+                  style: fontProvider.getSubTitleStyle(
+                    themeNotifier: themeNotifier,
+                  ),
                 ),
+                //! The value of the font chosen is selected to the origional selected font.
                 content: DropdownButton<String>(
-                  //! The value of the font chosen is selected to the origional selected font.
+                  dropdownColor: getContainerColor,
                   value: selectedFontFamily,
                   padding: EdgeInsets.zero,
                   //! Stack Overflow. (n.d.). flutter DropDownButton remove arrow icon? [online] Available at: https://stackoverflow.com/questions/67997560/flutter-dropdownbutton-remove-arrow-icon [Accessed 12 Feb. 2024].
@@ -99,7 +77,7 @@ class FontDropdownDialog extends StatelessWidget {
                       'Ok',
                       style: fontProvider
                           .smalltextfontstyle()
-                          .copyWith(color: Colors.red), // Use red color
+                          .copyWith(color: Colors.red),
                     ),
                   ),
                 ],
