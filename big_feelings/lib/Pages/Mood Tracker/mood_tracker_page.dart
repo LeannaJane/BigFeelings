2459a7d143e5aba18008103d6903b7cf3a936599
Leanna <leannajane2002@outlook.com>
2024-04-04 21:21:26 +0100

    Updated with descriptions of emotions.

--- big_feelings/lib/Pages/Mood Tracker/mood_tracker_page.dart
+++ big_feelings/lib/Pages/Mood Tracker/mood_tracker_page.dart
@@ -35,6 +35,29 @@ class _MoodTrackerPageState extends State<MoodTrackerPage> {
     'Upset',
     'Lonely'
   ];
+  //! Map to store descriptions for each mood
+  final Map<String, String> moodDescriptions = {
+    'Angry': 'Feeling really mad or upset.',
+    'Happy':
+        ' Feeling really good and joyful, like when you are laughing and smiling a lot.',
+    'Ok': 'Feeling alright, not too good, not too bad.',
+    'Overwhelmed':
+        'Feeling like there is too much going on all at once and it hard to handle.',
+    'Sad':
+        'Feeling unhappy or down, like when you might want to cry because something makes you feel bad or disappointed. It can also include feeling anxious or worried.',
+    'Sick': 'Feeling unwell or not very healthy.',
+    'Silly': 'Feeling playful or goofy.',
+    'Surprised': 'Feeling really amazed or shocked.',
+    'Tired': 'Feeling really sleepy or worn out.',
+    'Unhappy':
+        'Feeling unhappy or down, like when you might want to cry because something makes you feel bad or disappointed.',
+    'Upset': 'Feeling a bit bothered or troubled.',
+    'Lonely':
+        'Feeling like there\'s nobody around. Or feeling like you have no friends.'
+  };
+
+  bool showImage = true;
+  bool showDescription = false;
 
   @override
   void initState() {
@@ -101,6 +124,7 @@ class _MoodTrackerPageState extends State<MoodTrackerPage> {
       //! Increment the selectedEmotionIndex by 1 to move to the next emotion
       //! This uses a modulo (%) operator to make sure that the index remains within the bounds of the moods list
       selectedEmotionIndex = (selectedEmotionIndex + 1) % moods.length;
+      showDescription = false;
     });
   }
 
@@ -113,155 +137,197 @@ class _MoodTrackerPageState extends State<MoodTrackerPage> {
 
       selectedEmotionIndex =
           (selectedEmotionIndex - 1 + moods.length) % moods.length;
+      showDescription = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    //final User? user = FirebaseAuth.instance.currentUser;
     return Consumer<ThemeNotifier>(builder: (context, themeNotifier, child) {
       final fontProvider = Provider.of<FontProvider>(context);
       Color getContainerColor =
           Provider.of<ThemeNotifier>(context).getContainerColor();
       Color iconColor = themeNotifier.getIconColor();
 
-      /*
-     ! Below shows a page, that asks the user how they feel and presents the
-     ! users with 12 different emotional images. 
-     ! Each image is represented by an index.
-     ! References for the emotion images are in the reference page.
-     */
-
       return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            //! I made the title How do you feel.
-            'How do you feel?',
-            style: fontProvider.getOtherTitleStyle(themeNotifier),
-            //! I aligned the title to the center. And then set the fontweight to bald for now until I add the font style.
-            textAlign: TextAlign.center,
-          ),
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back,
-              size: 30.0,
-              color: iconColor,
+          appBar: AppBar(
+            title: Text(
+              'How do you feel?',
+              style: fontProvider.getOtherTitleStyle(themeNotifier),
+              textAlign: TextAlign.center,
             ),
-            onPressed: () {
-              //! If selected it will return to the previous page.
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //! Adding a forward and backwards icon to allow the user to scroll through the different emotions.
-            //! Ref 11.
-            Container(
-              //! The width and height of the container.
-              width: 250,
-              height: 250,
-              decoration: BoxDecoration(
-                //! The boarder radius styled.
-                borderRadius: BorderRadius.circular(30.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    spreadRadius: 1,
-                    blurRadius: 6,
-                    offset: const Offset(0, 0),
-                  ),
-                ],
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+                size: 30.0,
+                color: iconColor,
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Container(
-                  //! Added padding to the image to reduce the image size inside of the container.
-                  padding: const EdgeInsets.all(30),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: getContainerColor,
-                  ),
-                  child: Image.asset(
-                    'assets/images/images_mood/${moods[selectedEmotionIndex].toLowerCase()}.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: _navigateBackward,
-                  icon: Icon(
-                    Icons.arrow_back,
-                    size: 20,
-                    color: iconColor,
-                  ),
-                ),
-                const SizedBox(width: 30),
-                Text(
-                  moods[selectedEmotionIndex],
-                  style: fontProvider.subheadinglogin(themeNotifier),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(width: 30),
-                IconButton(
-                  onPressed: _navigateForward,
-                  icon: Icon(
-                    Icons.arrow_forward,
-                    size: 20,
-                    color: iconColor,
-                  ),
-                ),
-              ],
-            ),
-
-            //! Adding space between the mood label and the save mood button.
-            const SizedBox(height: 20.0),
-            //! I added a elevated button that allows the user to save their emotion by selecting the button. This saves to firebase.
-            GestureDetector(
-              onTap: () {
-                //! This accesses the current user ID
-                User? user = FirebaseAuth.instance.currentUser;
-                if (user != null) {
-                  _saveMoodToFirestore(moods[selectedEmotionIndex], user.uid,
-                      fontProvider, themeNotifier, context);
-                } else {
-                  logger.e('User is not logged in.');
-                  //! This handles when user not logged in
-                }
+              onPressed: () {
+                Navigator.pop(context);
               },
-              child: Container(
-                height: 40,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: getContainerColor,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 1,
-                      blurRadius: 6,
-                      offset: const Offset(0, 0),
+            ),
+          ),
+          body: Stack(children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showDescription = !showDescription;
+                      });
+                    },
+                    child: Container(
+                      width: 250,
+                      height: 250,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 6,
+                            offset: const Offset(0, 0),
+                          ),
+                        ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Container(
+                          padding: const EdgeInsets.fromLTRB(
+                              30, 20, 30, 30), // Adjusted padding
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: getContainerColor,
+                          ),
+                          child: showDescription
+                              ? Center(
+                                  child: Text(
+                                    moodDescriptions[
+                                            moods[selectedEmotionIndex]] ??
+                                        '',
+                                    style: fontProvider
+                                        .subheadinglogin(themeNotifier),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                )
+                              : Image.asset(
+                                  'assets/images/images_mood/${moods[selectedEmotionIndex].toLowerCase()}.png',
+                                  fit: BoxFit.contain,
+                                ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: getContainerColor,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 6,
+                            offset: const Offset(0, 0),
+                          ),
+                        ],
+                      ),
+                      child: IconButton(
+                        onPressed: _navigateBackward,
+                        icon: Icon(
+                          Icons.arrow_back,
+                          size: 20,
+                          color: iconColor,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 45,
+                      width: 120,
+                      child: Center(
+                        // Centering the text
+                        child: Text(
+                          moods[selectedEmotionIndex],
+                          style: fontProvider.subheadinglogin(themeNotifier),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: getContainerColor,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 6,
+                            offset: const Offset(0, 0),
+                          ),
+                        ],
+                      ),
+                      child: IconButton(
+                        onPressed: _navigateForward,
+                        icon: Icon(
+                          Icons.arrow_forward,
+                          size: 20,
+                          color: iconColor,
+                        ),
+                      ),
                     ),
                   ],
                 ),
-                alignment: Alignment.center,
-                child: Text(
-                  'Save Mood',
-                  style: fontProvider.subheadinglogin(themeNotifier),
+                SizedBox(height: 350.0),
+              ],
+            ),
+            Positioned(
+              bottom: 20,
+              left: MediaQuery.of(context).size.width / 2 - 75,
+              child: GestureDetector(
+                onTap: () {
+                  User? user = FirebaseAuth.instance.currentUser;
+                  if (user != null) {
+                    _saveMoodToFirestore(moods[selectedEmotionIndex], user.uid,
+                        fontProvider, themeNotifier, context);
+                  } else {
+                    logger.e('User is not logged in.');
+                  }
+                },
+                child: Container(
+                  height: 40,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    color: getContainerColor,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 6,
+                        offset: const Offset(0, 0),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Save Mood',
+                      style: fontProvider.subheadinglogin(themeNotifier),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
               ),
             )
-          ],
-        ),
-      );
+          ]));
     });
   }
 }
