aa012b50f153fbbd2391f979a115b48bd141adbf
Leanna <leannajane2002@outlook.com>
2024-04-06 02:28:59 +0100

    Adding a colour changer button

---
+++ big_feelings/lib/Pages/Library Page/Getting Help/colors_help.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:big_feelings/Classes/font_provider.dart';
+import 'package:big_feelings/Classes/theme_notifier.dart';
+
+class Colordialogcope extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeNotifier>(
+      builder: (context, themeNotifier, child) {
+        final fontProvider = Provider.of<FontProvider>(context);
+        final getContainerColor =
+            Provider.of<ThemeNotifier>(context).getContainerColor();
+
+        //! List of colours.
+        final List<Color> colors = [
+          Colors.red,
+          Colors.green,
+          Colors.blue,
+          const Color.fromARGB(255, 255, 90, 145),
+          const Color.fromARGB(255, 0, 195, 255),
+          const Color.fromARGB(255, 142, 7, 149),
+          getContainerColor,
+        ];
+
+        //! List of colour names.
+        final List<String> colorNames = [
+          'Red',
+          'Green',
+          'Blue',
+          'Pink',
+          'Baby Blue',
+          'Purple',
+          'Default',
+        ];
+
+        return Material(
+          color: Colors.black.withOpacity(0.5),
+          child: AlertDialog(
+            title: Text(
+              'Select a Colour',
+              textAlign: TextAlign.center,
+              style: fontProvider.subheadingBigBald(themeNotifier),
+            ),
+            backgroundColor: getContainerColor,
+            content: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: List.generate(colors.length, (index) {
+                  final color = colors[index];
+                  final colorName = colorNames[index];
+
+                  return GestureDetector(
+                    onTap: () => Navigator.of(context).pop(color),
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 6,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                            border: Border.all(
+                              color: Colors.transparent,
+                              width: 1, // Outline width
+                            ),
+                          ),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: color,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 5.0, horizontal: 10.0),
+                            width: 200,
+                            height: 50,
+                            child: Center(
+                              child: Text(
+                                colorName, // Set the color name
+                                style:
+                                    fontProvider.smalltextalert(themeNotifier),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- big_feelings/lib/Pages/Library Page/Getting Help/coping_methods.dart
+++ big_feelings/lib/Pages/Library Page/Getting Help/coping_methods.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: library_private_types_in_public_api, use_super_parameters
 import 'package:big_feelings/Classes/font_provider.dart';
 import 'package:big_feelings/Classes/theme_notifier.dart';
+import 'package:big_feelings/Pages/Library%20Page/Getting%20Help/colors_help.dart';
 import 'package:big_feelings/Pages/Library%20Page/Getting%20Help/list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:math' as math;
 
 class CopingMethods extends StatefulWidget {
   const CopingMethods({Key? key}) : super(key: key);
@@ -12,8 +14,17 @@ class CopingMethods extends StatefulWidget {
   _CopingMethodsState createState() => _CopingMethodsState();
 }
 
+//! Reusing code from the colors class from the minigames folder and editing for the change of colors for the cards.
 class _CopingMethodsState extends State<CopingMethods> {
   bool _isPaused = false;
+  late Color selectedColor;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColor =
+        Provider.of<ThemeNotifier>(context, listen: false).getContainerColor();
+  }
 
   void _togglePause() {
     setState(() {
@@ -36,6 +47,26 @@ class _CopingMethodsState extends State<CopingMethods> {
       final screenWidth = MediaQuery.of(context).size.width;
       final containerWidth = (screenWidth - 48) / 4;
       const containerHeight = 150.0;
+
+      String getColorName(Color color) {
+        if (color == Colors.red) {
+          return 'Red';
+        } else if (color == Colors.green) {
+          return 'Green';
+        } else if (color == Colors.blue) {
+          return 'Blue';
+        } else if (color == const Color.fromARGB(255, 255, 90, 145)) {
+          return 'Pink';
+        } else if (color == const Color.fromARGB(255, 0, 195, 255)) {
+          return 'Baby Blue';
+        } else if (color == const Color.fromARGB(255, 142, 7, 149)) {
+          return 'Purple';
+        } else if (color == getContainerColor) {
+          return 'Default';
+        } else {
+          return color.toString();
+        }
+      }
 
       return Scaffold(
         appBar: AppBar(
@@ -95,7 +126,7 @@ class _CopingMethodsState extends State<CopingMethods> {
                                   offset: const Offset(0, 3),
                                 ),
                               ],
-                              color: getContainerColor,
+                              color: selectedColor,
                             ),
                             child: _isPaused
                                 ? Center(
@@ -129,13 +160,84 @@ class _CopingMethodsState extends State<CopingMethods> {
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _togglePause,
-          tooltip: _isPaused ? 'Play' : 'Pause',
-          backgroundColor: getContainerColor,
-          child: Icon(
-            _isPaused ? Icons.play_arrow : Icons.pause,
-            color: iconColor,
+        bottomNavigationBar: Container(
+          height: 100,
+          color: Colors.transparent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                margin: const EdgeInsets.only(left: 20.0),
+                decoration: BoxDecoration(
+                  color: selectedColor,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                      offset: const Offset(0, 0),
+                    ),
+                  ],
+                ),
+                child: Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.rotationY(math.pi),
+                  child: IconButton(
+                    onPressed: _togglePause,
+                    tooltip: _isPaused ? 'Back' : 'Forward',
+                    icon: Icon(
+                      _isPaused ? Icons.arrow_back : Icons.arrow_forward,
+                      color: iconColor,
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  Color? newColor = await showDialog<Color>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Colordialogcope();
+                    },
+                  );
+
+                  if (newColor != null) {
+                    setState(() {
+                      selectedColor = newColor;
+                    });
+                  }
+                },
+                child: Center(
+                  child: Container(
+                    width: 150,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: selectedColor,
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 6,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        getColorName(selectedColor),
+                        textAlign: TextAlign.center,
+                        style: fontProvider.subheading(themeNotifier),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(width: 50), // Adding space to center the last element
+            ],
           ),
         ),
       );
@@ -147,12 +249,12 @@ class _CopingMethodsState extends State<CopingMethods> {
       int index,
       FontProvider fontProvider,
       ThemeNotifier themeNotifier,
-      getContainerColor) {
+      Color getContainerColor) {
     String copingMethod = copingMethods[index];
     String imagePath = copingMethodsWithImages[copingMethod] ?? '';
     TextStyle textStyle = fontProvider.description(themeNotifier);
-    TextStyle greenTextStyle =
-        textStyle.copyWith(color: Colors.green, fontWeight: FontWeight.bold);
+    TextStyle textstyle2 = fontProvider.close(themeNotifier);
+    TextStyle greenTextStyle = textstyle2.copyWith(fontWeight: FontWeight.bold);
 
     showDialog(
       context: context,
@@ -161,7 +263,7 @@ class _CopingMethodsState extends State<CopingMethods> {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.0),
           ),
-          backgroundColor: getContainerColor,
+          backgroundColor: selectedColor,
           child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
